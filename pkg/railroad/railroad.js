2df39943dd6984a17f4121e4157c405eaa10bd0c
Stefano Butera <Stefano Butera stefano.butera@nethnic.com>
2016-12-22 12:20:54 +0000

    0.0.6

--- pkg/railroad/railroad.js
+++ pkg/railroad/railroad.js
@@ -1,27 +1,58 @@
 'use strict';
 
-let grammkit = require('grammkit');
-let parse = require('pegjs/lib/parser').parse;
-let fs = require('fs');
-let hbs = require('handlebars');
+const grammkit = require('grammkit');
+const parse = require('pegjs/lib/parser').parse;
+const fs = require('fs');
+const hbs = require('handlebars');
 
-let template = hbs.compile(fs.readFileSync('pkg/railroad/railroad.hbs').toString('utf8'));
-let grammar = parse(fs.readFileSync('src/peg/crontalk.peg').toString('utf8'));
+const template = hbs.compile(fs.readFileSync('pkg/railroad/railroad.md.hbs').toString('utf8'));
+const grammar = parse(fs.readFileSync('src/peg/crontalk.peg').toString('utf8'));
 
 let rules = [];
-for (let rule of grammar.rules) {
-	try {
-		let svg = grammkit.diagram(rule);
-		rules.push({ 
-			name: rule.name,
-			rule: svg 
-		});
-	} catch (err) {
-		console.log(err);
+
+let ruleKeys = Object.keys(grammar.rules);
+
+let iterate = function(ruleKeys, grammarRules, tplRules, callback, ndx) {
+	let ruleIndex;
+	if (typeof (ndx) === 'undefined') {
+		ruleIndex = 0;
+	} else {
+		ruleIndex = ndx;
 	}
 
-}
+	let rule = grammarRules[ruleKeys[ruleIndex]];
 
-var out = template({ rules: rules });
+	try {
+		let svg = grammkit.diagram(rule);
 
-fs.writeFileSync('docs/railroad.html', out);
+		fs.writeFile('docs/svg/' + rule.name + '.svg', svg, function(err) {
+			if (err !== null) {
+				console.log(err);
+			}
+
+			tplRules.push({ 
+				name: rule.name,
+				rule: svg 
+			});
+
+			if (ruleIndex < ruleKeys.length - 1) {
+				setImmediate(iterate, ruleKeys, grammarRules, tplRules, callback, ruleIndex + 1);
+			} else {
+				setImmediate(callback, tplRules);
+			}
+		});
+	} catch (err) {
+		if (ruleIndex < ruleKeys.length - 1) {
+			setImmediate(iterate, ruleKeys, grammarRules, tplRules, callback, ruleIndex + 1);
+		} else {
+			setImmediate(callback, tplRules);
+		}
+	}
+};
+
+let tplRules = [];
+
+iterate(ruleKeys, grammar.rules, tplRules, function(rules) {
+	let out = template({ rules: rules });
+	fs.writeFileSync('docs/railroad.md', out);
+});
